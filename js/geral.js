00946b02f50783011ca4c21a8b059c4d103b5755
Ronneesley <ronneesley@gmail.com>
2024-03-26 19:35:19 -0300

    PDI

--- js/geral.js
+++ js/geral.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         $("#conteudo").load(href, function(){
             $("#conteudo a").click(processarLink);
 
+            carregamentoCodigos();
             hljs.highlightAll();
         });
     }
@@ -13,6 +14,23 @@ $(document).ready(function(){
 
             return false;
         }
+    }
+
+    function carregamentoCodigos(){
+        $("code").each(function(index){
+            var codigo = $(this);
+            var pedacos = $(this).attr("class").split(" ")
+
+            for (let i = 0; i < pedacos.length; i++){
+                let pedaco = pedacos[i];
+                
+                if (pedaco.startsWith("codigo:")){
+                    let caminho = pedaco.substring(7);
+
+                    codigo.load(caminho);
+                }
+            }
+        });
     }
 
     $("a").click(processarLink);
